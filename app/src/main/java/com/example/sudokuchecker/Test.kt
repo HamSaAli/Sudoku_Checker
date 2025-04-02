38377d61a73e9eb86eb9c2d7733559279feb6ee9
hamsa <hamssaalsaadie@gmail.com>
2025-04-02 16:56:00 +0300

    edit Boards.kt

--- app/src/main/java/com/example/sudokuchecker/Test.kt
+++ app/src/main/java/com/example/sudokuchecker/Test.kt
@@ -13,59 +13,41 @@ fun main(){
         correctResult = true
     )
     test(
-        name = "Test 3 - when board have duplicate numbers in one column should return false ",
+        name = "Test 3 - when board have duplicate numbers in column should return false ",
         result = sudokuChecker(generateBoard(3)),
-        correctResult = false
-    )
-    test(
-        name = "Test 4 - when board have duplicate numbers in multiple column should return false ",
-        result = sudokuChecker(generateBoard(4)),
-        correctResult = false
-    )
-    test(
-        name = "Test 5 - when board have duplicate numbers in one row should return false ",
-        result = sudokuChecker(generateBoard(5)),
-        correctResult = false
-    )
-    test(
-        name = "Test 6 - when board have duplicate numbers in multiple row should return false ",
-        result = sudokuChecker(generateBoard(6)),
-        correctResult = false
-    )
-    test(
-        name = "Test 7 - when board have duplicate numbers in 3*3 subgrid should return false ",
-        result = sudokuChecker(generateBoard(7)),
-        correctResult = false
-    )
-    test(
-        name = "Test 8 - when board have a spaces and the board has duplicates should return false",
-        result = sudokuChecker(generateBoard(8)),
-        correctResult = false
-    )
-    test(
-        name = "Test 9 - when board have duplicates in multiple rows,columns and box should return false",
-        result = sudokuChecker(generateBoard(9)),
-        correctResult = false
-    )
-    test(
-        name = "Test 10 - when board have just empty cells with no numbers should return true",
-        result = sudokuChecker(generateBoard(10)),
-        correctResult = true
-    )
-    test(
-        name = "Test 11 - when board have duplicates both row and column should return false",
-        result = sudokuChecker(generateBoard(11)),
-        correctResult = false
-    )
-    test(
-        name = "Test 12 - when board have duplicates in multiple rows,columns and box and empty cells should return false",
-        result = sudokuChecker(generateBoard(12)),
         correctResult = false
     )
 
     test(
-        name = "Test 13 - when board have missing row or col should return false",
-        result = sudokuChecker(generateBoard(13)),
+        name = "Test 4 - when board have duplicate numbers in row should return false ",
+        result = sudokuChecker(generateBoard(4)),
+        correctResult = false
+    )
+
+    test(
+        name = "Test 5 - when board have duplicate numbers in 3*3 subgrid should return false ",
+        result = sudokuChecker(generateBoard(5)),
+        correctResult = false
+    )
+    test(
+        name = "Test 6 - when board have a spaces and the board has duplicates should return false",
+        result = sudokuChecker(generateBoard(6)),
+        correctResult = false
+    )
+    test(
+        name = "Test 7 - when board have duplicates in multiple rows,columns and box should return false",
+        result = sudokuChecker(generateBoard(7)),
+        correctResult = false
+    )
+    test(
+        name = "Test 8 - when board have just empty cells with no numbers should return true",
+        result = sudokuChecker(generateBoard(8)),
+        correctResult = true
+    )
+
+    test(
+        name = "Test 9 - when board have missing row or col should return false",
+        result = sudokuChecker(generateBoard(9)),
         correctResult = false
     )
 
@@ -74,7 +56,7 @@ fun main(){
 
 fun test(name: String, result:Boolean, correctResult:Boolean){
     if (result == correctResult){
-        println("Passed - $name - $result")
+        println("Passed - $name - Result : $result")
     } else{
         println("Failed - $name - $result")
     }
