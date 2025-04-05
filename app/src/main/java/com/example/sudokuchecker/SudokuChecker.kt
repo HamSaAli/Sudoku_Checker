e56fc13d3974d7c6d8349aae4bd7af94f1f21dc4
hamsa <hamssaalsaadie@gmail.com>
2025-04-05 13:31:44 +0300

    update Boards.kt to be list<list<string>>

--- app/src/main/java/com/example/sudokuchecker/SudokuChecker.kt
+++ app/src/main/java/com/example/sudokuchecker/SudokuChecker.kt
@@ -2,37 +2,37 @@ package com.example.sudokuchecker
 
 import kotlin.math.sqrt
 
-fun sudokuChecker(sudokuBoard: List<String>): Boolean {
+fun sudokuChecker(sudokuBoard: List<List<String>>): Boolean {
     val boardSize = sudokuBoard.size
     val boxSize = sqrt(boardSize.toDouble()).toInt()
-    val allowedChars = ('1'..'9').toSet() + setOf('-')
+    val allowedChars = ('1'..'9').map { it.toString() }.toSet() + setOf("-")
 
     for (row in sudokuBoard) {
-        if (row.length != boardSize) return false
+        if (row.size != boardSize) return false
         if (row.any { it !in allowedChars }) return false
     }
 
     for (index in 0 until boardSize) {
-        val rowSet = mutableSetOf<Char>()
-        val columnSet = mutableSetOf<Char>()
+        val rowSet = mutableSetOf<String>()
+        val columnSet = mutableSetOf<String>()
 
         for (secondIndex in 0 until boardSize) {
             val rowChar = sudokuBoard[index][secondIndex]
             val columnChar = sudokuBoard[secondIndex][index]
 
-            if (rowChar != '-' && !rowSet.add(rowChar)) return false
-            if (columnChar != '-'  && !columnSet.add(columnChar)) return false
+            if (rowChar != "-" && !rowSet.add(rowChar)) return false
+            if (columnChar != "-" && !columnSet.add(columnChar)) return false
         }
     }
 
     for (rowStart in 0 until boardSize step boxSize) {
         for (colStart in 0 until boardSize step boxSize) {
-            val boxSet = mutableSetOf<Char>()
+            val boxSet = mutableSetOf<String>()
 
             for (index in 0 until boxSize) {
                 for (secondIndex in 0 until boxSize) {
                     val boxChar = sudokuBoard[rowStart + index][colStart + secondIndex]
-                    if (boxChar != '-' && !boxSet.add(boxChar)) return false
+                    if (boxChar != "-" && !boxSet.add(boxChar)) return false
                 }
             }
         }
